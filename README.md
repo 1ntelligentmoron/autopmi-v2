43b0ac873f4ff2b8ccbbb76f2784f340b328ff53
Leung Lok Hay Hayden <lokhayhl966@gmail.com>
2022-01-22 01:53:55 +0800

    Feature complete

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 1ntelligentMoron's AutoPMI V2
-Version 1 has reached EOL for full-day school resumption in Oct 2021.
+Version 1 has reached EOL since full-day school resumption in Oct 2021.
 
 # What does this app do?
 AutoPMI is a tool for Logos IB M23 Cohort (i.e. 21-22 MS3) students to join Zoom lessons automatically, discarding the need to look up which teacher's class to join, finding the teacher's PMI, and entering the PMI in Zoom. Contact me through any means if you have problems.
---
+++ app.py
@@ -0,0 +1,119 @@
+from datetime import datetime
+import time, webbrowser, pyautogui
+dt = lambda fmt: datetime.now().strftime(str(fmt))
+
+# Read pmi.txt and load as variable
+def PMI_ALL(path='pmi.txt'):
+    pmi = {}
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            pmi_pair = line.split('=')
+            if pmi_pair[0][0] == '#':
+                continue
+            pmi_pair[1] = pmi_pair[1].split('\n')[0]
+            pmi[pmi_pair[0]] = pmi_pair[1]
+    return pmi
+
+
+# Determine upcoming lesson slot
+# def lesson(t=int(dt('%H%M')), dow=int(dt('%w'))):
+def lesson(t=950, dow=2):  # Testing
+    slot = -1
+    if 730 <= t < 825:
+        slot = 0
+    elif 825 <= t < 900:
+        slot = 1
+    elif 900 <= t < 940:
+        slot = 2
+    elif 940 <= t < 1035:
+        slot = 3
+    elif 1035 <= t < 1115:
+        slot = 4
+    elif 1115 <= t < 1210:
+        slot = 5
+    elif 1210 <= t < 1345:
+        slot = 6
+    elif 1345 <= t < 1420:
+        slot = 7
+    elif 1420 <= t < 1505:
+        slot = 8
+    elif 1505 <= t < 1540:
+        slot = 9
+    elif 1540 <= t < 1635:
+        slot = 10
+    else:
+        return
+    
+    lsn = (dow, slot)
+    if lsn in ((1, 0), (2, 0), (5, 0)):
+        return
+    return lsn
+
+
+# Determine subject by DoW and slot
+def subj(lesson_ident: tuple):
+    dow = lesson_ident[0]
+    slot = lesson_ident[1]
+    subjs = (
+        ('', '', 'HRT', 'HRT', '',),
+        ('ENG', 'C_SL', 'TOK', 'ENG', 'ASSEMBLY',),
+        ('CHI', 'C_SL', 'B_SL', 'ENG', 'C_SL',),
+        ('CHI', 'MATHS_SL/HL', 'B_SL', 'B_SL', 'C_SL',),
+        # Recess
+        ('TOK', 'CHI', 'B_SL', 'B_SL', 'C_SL',),
+        ('TOK', 'CHI', 'CHI', 'MATHS_SL/HL', 'PE',),
+        # Lunch
+        ('MATHS_SL/HL', 'A_SL', 'ENG', 'A_SL', 'ENG',),
+        ('MATHS_SL/HL', 'A_SL', 'ENG', 'A_SL', 'MATHS_SL/HL',),
+        # Break
+        ('PE', 'ENG', 'REFLE', 'A_SL', 'CHI',),
+        ('A_HL', 'B_HL', 'MATHS_HL', 'C_HL', 'CHI',),
+        ('A_HL', 'B_HL', 'MATHS_HL', 'C_HL', 'EE',),
+    )
+    return subjs[slot][dow-1]
+
+
+# Join Zoom automatically if able, else show message
+def join(subj: str):
+    
+    if subj == 'REFLE':
+        print('Please choose the subject manually, this window will close in 30 seconds.')
+        print('RE: https://zoom.us/j/4287727803')
+        print('FLE: https://zoom.us/j/5168805269')
+        time.sleep(30)
+        return
+    
+    pmi = PMI_ALL()[subj]
+    if 9 <= len(pmi) <= 11:
+        halt = input(f'Joining {subj} lesson, confirm? (Press ENTER to continue, close this window if not.)')
+        url = f'https://zoom.us/j/{pmi}'
+        webbrowser.open(url)
+        time.sleep(5)  # Wait for browser to load page
+        pyautogui.press('left')
+        pyautogui.press('enter')
+        return
+    
+    if subj[-3:] == '_HL':
+        print('No upcoming lesson. Closing window in 3 seconds.')
+        time.sleep(3)
+        return
+    
+    print('Invalid PMI in pmi.txt! Please correct accordingly.')
+    time.sleep(3)
+    return
+
+
+# Script
+def main():
+    
+    if not lesson():
+        print('No upcoming lesson. Closing window in 3 seconds.')
+        time.sleep(3)
+        return
+    
+    join(subj(lesson()))
+    return
+
+
+if __name__ == '__main__':
+    main()
